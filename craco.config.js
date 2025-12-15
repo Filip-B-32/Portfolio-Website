9bb06642bd06668ee16ff52e8c2e71ce5fcc8cc2
Bogdan Filip <bfilip32@yahoo.com>
2025-12-15 18:34:18 +0200

    fix6

--- craco.config.js
+++ craco.config.js
@@ -6,13 +6,22 @@ module.exports = {
       "@": path.resolve(__dirname, "src"),
       "@assets": path.resolve(__dirname, "src", "assets"),
     },
+    configure: (webpackConfig) => {
+      // Disable ESLint plugin to avoid warning
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        plugin => plugin.constructor.name !== 'ESLintWebpackPlugin'
+      );
+      return webpackConfig;
+    }
   },
   style: {
     postcss: {
+      mode: 'extends',
       loaderOptions: {
         postcssOptions: {
           plugins: [
-            require('postcss-calc')({ preserve: true }), // preserve invalid functions
+            // Remove postcss-calc to prevent CSS variable processing issues
+            // The browser will handle the calc() with CSS variables at runtime
             require('autoprefixer'),
           ],
         },
